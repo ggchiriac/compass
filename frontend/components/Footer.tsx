4c84feeaa4aa27d06d485ccfd83389c1c2dfa7d7
Windsor Nguyen <93564933+windsornguyen@users.noreply.github.com>
2024-02-19 20:08:28 -0500

    week 3

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -34,7 +34,7 @@ export default function Footer() {
         </div>
         <div className='mt-8 md:order-1 md:mt-0'>
           <p className='text-center text-xs leading-5 text-gray-500'>
-            Princeton Compass &copy; 2023.
+            Princeton Compass &copy; 2024.
           </p>
         </div>
       </div>
